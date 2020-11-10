8c8deac03eef28aa2cc4f22973306d929d357eb1
JulieBoberg <43385336+JulieBoberg@users.noreply.github.com>
2020-11-10 08:50:04 -0800

    custom search overlay output

--- inc/search-routes.php
+++ inc/search-routes.php
@@ -9,8 +9,64 @@ function universityRegisterSearch(){
     ));
 }
 
-function universitySearchResults(){
-    return 'Congratulations you created a route'; 
+function universitySearchResults($data){
+   $mainQuery = new WP_Query(array(
+       'post_type' => array('post', 'page', 'professor', 'event', 'program'),
+       's' => sanitize_text_field($data['term']) 
+   ));
+   
+   $results = array(
+       'generalInfo' => array(),
+       'professors'=> array(),
+       'programs' => array(),
+       'events' => array()
+   );
+
+while ($mainQuery-> have_posts()){
+    $mainQuery->the_post();
+
+    if(get_post_type() == 'post' OR get_post_type() == 'page'){
+        array_push($results['generalInfo'], array(
+            'postType' => get_post_type(),
+            'title' => get_the_title(),
+            'permalink' => get_the_permalink(),
+            'authorName' => get_the_author()
+        ));
+    }
+    if(get_post_type() == 'professor'){
+        array_push($results['professors'], array(
+            'title' => get_the_title(),
+            'permalink' => get_the_permalink(),
+            'image' => get_the_post_thumbnail_url(0, 'professorLandscape')
+        ));
+    }
+    if(get_post_type() == 'program'){
+        array_push($results['programs'], array(
+            'title' => get_the_title(),
+            'permalink' => get_the_permalink()
+        ));
+    }
+    if(get_post_type() == 'event'){
+        $eventDate = new DateTime(get_field('event_date'));
+$excerpt = null;
+if(has_excerpt()){
+    $excerpt = get_the_excerpt();
+  }else{
+    $excerpt = wp_trim_words(get_the_content(), 18);
+  }
+        array_push($results['events'], array(
+            'title' => get_the_title(),
+            'permalink' => get_the_permalink(),
+            'month' => $eventDate -> format('M'),
+            'day' => $eventDate -> format('d'),
+            'excerpt' => $excerpt
+        ));
+    }
+    
+}
+
+
+   return $results;
 }
 
 
--- js/modules/Search.js
+++ js/modules/Search.js
@@ -50,24 +50,57 @@ this.previousValue = this.searchField.val();
 }
 
 getResults(){
-    $.when(
-        $.getJSON(universityData.root_url + '/wp-json/wp/v2/posts?search=' + this.searchField.val()),
-         $.getJSON(universityData.root_url + '/wp-json/wp/v2/pages?search=' + this.searchField.val())
-         ).then((posts, pages)=>{
-        var combinedResults = posts[0].concat(pages[0])
-        
-        this.resultsDiv.html(`
-        <h2 class="search-overlay__section-title">General Information</h2>
-       ${combinedResults.length ? ' <ul class="link-list min-list">' : '<p>No general information matches that search term</p>' }
+    $.getJSON(universityData.root_url + '/wp-json/university/v1/search?term=' + this.searchField.val(), (results)=>{
+ this.resultsDiv.html(`
+    <div class="row">
+    <div class="one-third">
+    <h2 class="search-overlay__section-title">General Information</h2>
+    ${results.generalInfo.length ? ' <ul class="link-list min-list">' : '<p>No general information matches that search term</p>' }
        
-        ${combinedResults.map(post => `<li><a href="${post.link}">${post.title.rendered}</a>
-        ${post.type == 'post' ? `by ${post.authorName}` : '' }</li>`).join('')}
-       ${combinedResults.length ? '</ul>' : ''} 
-        `)
-        this.isSpinnerVisible = false;
+    ${results.generalInfo.map(post => `<li><a href="${post.permalink}">${post.title}</a>
+    ${post.postType == 'post' ? `by ${post.authorName}` : '' }</li>`).join('')}
+   ${results.generalInfo.length ? '</ul>' : ''} 
 
-    },() => {
-        this.resultsDiv.html('<p>Unexpected Error; Please try again</p>')
+    </div>
+    <div class="one-third">
+    <h2 class="search-overlay__section-title">Programs</h2>
+    ${results.programs.length ? ' <ul class="link-list min-list">' : `<p>No programs match that search. <a href="${universityData.root_url}/programs" >View all programs</a></p>` }
+    ${results.programs.map(post => `<li><a href="${post.permalink}">${post.title}</a>
+    </li>`).join('')}
+   ${results.programs.length ? '</ul>' : ''} 
+    <h2 class="search-overlay__section-title">Professors</h2>
+
+    ${results.professors.length ? ' <ul class="professor-cards">' : `<p>No professors match that search. </p>` }
+    ${results.professors.map(post => `
+    <li class="professor-card__list-item">
+    <a class="professor-card" href="${post.permalink}"> 
+    <img class="professor-card__image" src="${post.image}">
+    <span class="professor-card__name">${post.title}</span>
+  </a></li>
+    `).join('')}
+   ${results.professors.length ? '</ul>' : ''} 
+
+    </div>
+    <div class="one-third">
+    <h2 class="search-overlay__section-title">Events</h2>
+    ${results.events.length ? '' : `<p>No events match that search. <a href="${universityData.root_url}/events" >View all events</a></p>` }
+    ${results.events.map(post => `
+    <div class="event-summary">
+            <a class="event-summary__date t-center" href="${post.permalink}">
+              <span class="event-summary__month">${post.month}</span>
+              <span class="event-summary__day">${post.day}</span>
+            </a>
+            <div class="event-summary__content">
+              <h5 class="event-summary__title headline headline--tiny"><a href="${post.permalink}">${post.title}</a></h5>
+              <p> ${post.excerpt} <a href="${post.permalink}" class="nu gray">Learn more</a></p>
+            </div>
+          </div>
+    `).join('')}
+  
+    </div>
+    </div>
+    `);
+    this.isSpinnerVisible = false;
     });
 }
 
