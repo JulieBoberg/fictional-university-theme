85c29d40d50adfd65f1ecc72639e79421f3568e0
JulieBoberg <43385336+JulieBoberg@users.noreply.github.com>
2020-11-18 16:16:38 -0800

    minor fix to functions page

--- functions.php
+++ functions.php
@@ -173,6 +173,11 @@ function makeNotePrivate($data, $postarr){
   return $data;
 }
 
+add_filter('ai1wm_exclude_content_from_export','ignoreCertainFiles');
 
+function ignoreCertainFiles($exclude_filters){
+   $exclude_filters[] = 'themes/fictional-university-theme/node_modules';
+   return $exclude_filters;
+}
 ?>
 
